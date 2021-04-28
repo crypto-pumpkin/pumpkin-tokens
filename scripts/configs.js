d44eec993ce851f5db434cf6f8aa39d13de0d814
crypto-pumpkin <cryptopumpkin2020@gmail.com>
2021-04-27 23:02:48 -0700

    add vars into deploy configs

--- scripts/configs.js
+++ scripts/configs.js
@@ -3,9 +3,13 @@ require("dotenv").config();
 module.exports = {
 	42: {
 		network: 'Kovan',
+		dai: process.env.KOVAN_DAI,
+		wbtc: process.env.KOVAN_WBTC,
 	},
 	3: {
     env: 'Ropsten',
+		dai: process.env.ROPSTEN_DAI,
+		wbtc: process.env.ROPSTEN_WBTC,
   },
 	1: {
 		network: 'Mainnet',
